5ad88d6e79a5379048fb33c1174f87fa4cfd795e
alfairy <chris@jzoo.om>
2022-08-03 21:06:14 +1000

    Another commit

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import react from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, Image, SafeAreaView } from "react-native";
 
 export default function App() {
+  console.log("App executed");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Hello React Native</Text>
+      <Image
+        source={{
+          width: 100,
+          height: 100,
+          uri: "https://picsum.photos/100/100",
+        }}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
